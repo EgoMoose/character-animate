d567c436876676ee00fbc1c93eb6583a6076b68e
EgoMoose <EgoMoose@users.noreply.github.com>
2023-05-06 21:25:05 -0400

    Strict typing fixes

--- src/init.lua
+++ src/init.lua
@@ -4,21 +4,21 @@ local AnimateR6 = require(script:WaitForChild("AnimateR6"))
 local AnimateR15 = require(script:WaitForChild("AnimateR15"))
 local ManualDirector = require(script:WaitForChild("ManualDirector"))
 
-type Animate = {
+type AnimateController = {
 	cleanup: () -> (),
 	playEmote: (string | Animation) -> (boolean, AnimationTrack?)
 }
 
 type ManualHumanoid = ManualDirector.ManualHumanoid
 
-type AnimateManually = Animate & {
+type AnimateControllerManually = AnimateController & {
 	fireState: ManualDirector.FireState,
 	setMovement: ManualDirector.SetMovement,
 }
 
 local module = {}
 
-local function animateInternal(parent: Instance, director: Humanoid | ManualHumanoid, performer: Humanoid?): Animate
+local function animateInternal(parent: Instance, director: Humanoid | ManualHumanoid, performer: Humanoid?): AnimateController
 	local castedDirector = director :: Humanoid
 	local actor = performer or castedDirector
 
@@ -35,11 +35,11 @@ local function animateInternal(parent: Instance, director: Humanoid | ManualHuma
 	}
 end
 
-function module.animate(parent: Instance, director: Humanoid, performer: Humanoid?): Animate
+function module.animate(parent: Instance, director: Humanoid, performer: Humanoid?): AnimateController
 	return animateInternal(parent, director, performer)
 end
 
-function module.animateManually(parent: Instance, performer: Humanoid): AnimateManually
+function module.animateManually(parent: Instance, performer: Humanoid): AnimateControllerManually
 	local director = ManualDirector.create()
 	local animated = animateInternal(parent, director.humanoid, performer)
 
